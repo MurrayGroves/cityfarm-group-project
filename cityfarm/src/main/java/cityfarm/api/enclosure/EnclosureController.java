eac8df845d2f0fa1c1c63b7807709cf9122999c7
Murray Groves <github@murraygrov.es>
2023-11-15 13:08:25 +0000

    Add GET endpoints

--- cityfarm/src/main/java/cityfarm/api/enclosure/EnclosureController.java
+++ cityfarm/src/main/java/cityfarm/api/enclosure/EnclosureController.java
@@ -1,16 +1,12 @@
 package cityfarm.api.enclosure;
 
-import cityfarm.api.AnimalRepository;
-import cityfarm.api.Cow;
-import cityfarm.api.CowGeneric;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class EnclosureController {
@@ -29,5 +25,28 @@ public class EnclosureController {
 
         String location = String.format("/enclosures/by_id/%s", enclosure.get_id());
         return ResponseEntity.created(URI.create(location)).body(enclosure);
+    }
+
+    @GetMapping("/api/enclosures")
+    public List<EnclosureGeneric> get_enclosures() {
+        return enclosureRepository.findAll();
+    }
+
+    @GetMapping("/api/enclosures/by_id/{id}")
+    public ResponseEntity<EnclosureGeneric> get_enclosure_by_id(@PathVariable String id) {
+        EnclosureGeneric enclosure = enclosureRepository.findEnclosureById(id);
+
+        if (enclosure == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(enclosure);
+    }
+
+    @GetMapping("/api/enclosures/by_name/{name}")
+    public ResponseEntity<List<EnclosureGeneric>> get_enclosure_by_name(@PathVariable String name) {
+        List<EnclosureGeneric> enclosure = enclosureRepository.findEnclosureByName(name);
+
+        return ResponseEntity.ok().body(enclosure);
     }
 }
