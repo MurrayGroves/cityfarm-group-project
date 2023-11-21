d4b9b26051e2e0126014871a35f9a8dc914ba46b
LiamBristolAccount <124677437+LiamBristolAccount@users.noreply.github.com>
2023-11-21 18:42:04 +0000

    Add calendar, NavBar, AnimalTable, SearchBar files
    add functionality for a navigation bar for pages in the app, a table for displaying the animals from the api, a basic template for a calendar for displaying events and part of a search bar for searching through the animal table.

---
+++ cityfarmreact/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import './NavBar.css'
+import {Link} from "react-router-dom";
+
+const NavBar = ()   => {
+    const [open, setOpen] = useState(false); {/*Navbar starts closed*/}
+    const openNavbar = () => {
+        {open ? setOpen(false) : setOpen(true)} {/*switches the value of open between true and false*/}
+    }
+
+    if (open) {
+        return(
+        <div id="sidebar" className="sidebar" style={{width: 250}}>
+            <a href="javascript:void(0)" onClick={() => openNavbar()}>×</a>
+            <Link to="/"> Home </Link>
+            <Link to="/calendar"> Calendar </Link>
+            <Link to="/animals"> Animals </Link>
+            <Link to="/create"> AddAnimal </Link>
+        </div>
+        )
+    }
+    return (
+        <button className="openbtn" onClick={() => openNavbar()}>☰ </button>
+    );
+}
+export default NavBar;
---
+++ cityfarmreact/src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+import baseURL from "../api/axiosConfig";
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchAnimals = async (animalID) => {
+        const response = await  fetch(`${baseURL}animals/by_id/${searchTerm}`);
+        const data = await response.json();
+        setSearchTerm(data)
+    }
+    useEffect(() => {
+        searchAnimals(searchTerm);
+    },[]);
+    return(
+        <div className="app">
+            <h1>App</h1>
+            <div className="search">
+                <input
+                    placeholder="Search for animals"
+                    value= {searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} {/*Sets the "event" to the search term (basically anything in the searchbar gets set to this value)*/}
+                />
+                <img src={SearchIcon} {/*TODO: Get an image of a magnifying glass for the searchBar*/}
+                     alt="search" {/*Alternative tag is useful for screen readers */}
+                     onClick={() => searchAnimals(searchTerm)} {/*calls the search function to find an animal*/}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
