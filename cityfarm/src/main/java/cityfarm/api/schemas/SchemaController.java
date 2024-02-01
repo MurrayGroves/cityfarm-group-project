14041eb17c1b73c9c34d41df69f5e47ba5932984
Murray Groves <github@murraygrov.es>
2024-02-01 18:12:24 +0000

    Add schema GET endpoints

--- cityfarm/src/main/java/cityfarm/api/schemas/SchemaController.java
+++ cityfarm/src/main/java/cityfarm/api/schemas/SchemaController.java
@@ -1,11 +1,13 @@
 package cityfarm.api.schemas;
 
+import cityfarm.api.animals.AnimalGeneric;
 import cityfarm.api.animals.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000", "https://cityfarm.murraygrov.es"}, methods = {RequestMethod.GET, RequestMethod.POST})
@@ -19,5 +21,21 @@ public class SchemaController {
 
         String location = String.format("/api/schemas/by_name/%s", schema.get_name());
         return ResponseEntity.created(URI.create(location)).body(schema);
+    }
+
+    @GetMapping("/api/schemas")
+    public ResponseEntity<List<AnimalSchema>> get_schemas() {
+        return ResponseEntity.ok().body(schemaRepository.findAll());
+    }
+
+    @GetMapping("/api/schemas/by_name/{name}")
+    public ResponseEntity<AnimalSchema> by_name(@PathVariable String name) {
+        AnimalSchema schema = schemaRepository.findSchemaByName(name);
+
+        if (schema == null) {
+            return ResponseEntity.status(404).build();
+        }
+
+        return ResponseEntity.ok().body(schema);
     }
 }
