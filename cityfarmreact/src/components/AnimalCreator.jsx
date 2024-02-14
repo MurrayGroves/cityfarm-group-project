d10ae5781605f5c435b517afefd93326d08b4621
sd22404 <sd22404@bristol.ac.uk>
2024-02-14 13:14:59 +0000

    placeholder new animal selection fields

--- cityfarmreact/src/components/AnimalCreator.jsx
+++ cityfarmreact/src/components/AnimalCreator.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from "../api/axiosConfig";
 import '../pages/AnimalTable.css';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -12,12 +13,26 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete'
+import DeleteIcon from '@mui/icons-material/Delete';
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const AnimalCreator = () => {
 
-    const [create, setCreate] = useState({name: '', type: '', father: 'Unregistered', mother: 'Unregistered', male: 'true', alive: 'true'})
+    const [newAnimal, setNewAnimal] = useState({name: '', type: 'Pig', father: 'Unregistered', mother: 'Unregistered', male: 'true', alive: 'true'})
+    const [schemaList, setSchemaList] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(`/schemas`);
+                console.log(response.data);
+                setSchemaList(response.data.reverse());
+            } catch (error) {
+                window.alert(error);
+            }
+        })()
+    },[])
 
     return (
         <TableContainer component={Paper}>
@@ -33,11 +48,34 @@ const AnimalCreator = () => {
                 </TableHead>
                 <TableBody>
                     <TableRow>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
+                        <TableCell><TextField onChange={(e)=>{setNewAnimal({...newAnimal, name: e.target.value})}}></TextField></TableCell>
+                        <TableCell>
+                            <Select value={newAnimal.type} onChange={(e) => {setNewAnimal({...newAnimal, type: e.target.value})}}>
+                            {schemaList.map((schema) => {
+                                return <MenuItem value={schema._name}>{schema._name}</MenuItem>
+                            })}
+                            </Select>
+                        </TableCell>
+                        <TableCell>
+                            <Autocomplete
+                                size='medium'
+                                renderInput={(params) => <TextField {...params} label="Father"/>}
+                                options={[]}
+                            />
+                        </TableCell>
+                        <TableCell>
+                            <Autocomplete
+                                size='medium'
+                                renderInput={(params) => <TextField {...params} label="Mother"/>}
+                                options={[]}
+                            />
+                        </TableCell>
+                        <TableCell>
+                            <Select value={newAnimal.male} onChange={(e) => {setNewAnimal({...newAnimal, male: e.target.value})}}>
+                                <MenuItem value={true}>Male</MenuItem>  
+                                <MenuItem value={false}>Female</MenuItem>
+                            </Select>
+                        </TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
