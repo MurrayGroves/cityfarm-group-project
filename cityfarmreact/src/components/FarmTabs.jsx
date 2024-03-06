1264cce5af5e132f6f79d85d1cc403a9c0c5a642
sd22404 <sd22404@bristol.ac.uk>
2024-03-06 17:46:46 +0000

    adjust tab colours slightly

--- cityfarmreact/src/components/FarmTabs.jsx
+++ cityfarmreact/src/components/FarmTabs.jsx
@@ -8,13 +8,15 @@ import { createTheme } from "@mui/material";
 const FarmTabs = (props) => {
     const farms = props.farms;
 
-    var tabTheme = createTheme({palette: {mode: useTheme().palette.mode}});
+    var tabTheme = createTheme(useTheme());
     var mainTheme = useTheme();
 
-    if(!props.selectedFarm) {
-        tabTheme = mainTheme;
-    } else {
+    if (props.selectedFarm) {
         tabTheme.palette.primary.main = mainTheme.palette[farms[props.selectedFarm]].main;
+    } else {
+        if (props.selectedFarm != null) {
+            tabTheme.palette.primary.main = mainTheme.palette.green.main;
+        }
     }
 
     return (
@@ -22,10 +24,10 @@ const FarmTabs = (props) => {
             <ThemeProvider theme={tabTheme}>
                 <Tabs value={props.selectedFarm} onChange={(e, farm)=>{props.setSelectedFarm(farm)}}>
                     <Tab value={null} label="All"/>
-                    <Tab value={''} label="Other"/>
                     <Tab value={farms.WH} label="Windmill Hill"/>
                     <Tab value={farms.HC} label="Hartcliffe"/>
                     <Tab value={farms.SW} label="St Werburghs"/>
+                    <Tab value={''} label="Other"/>
                 </Tabs>
             </ThemeProvider>
         </div>
