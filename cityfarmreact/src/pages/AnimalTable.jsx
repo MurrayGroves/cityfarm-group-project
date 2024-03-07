eafc239eabb63ab3db668797c94e11bf2ded8507
Murray Groves <github@murraygrov.es>
2024-03-07 14:43:03 +0000

    Add edit button to each row, makes each cell editable

--- cityfarmreact/src/pages/AnimalTable.jsx
+++ cityfarmreact/src/pages/AnimalTable.jsx
@@ -3,16 +3,25 @@ import axios from '../api/axiosConfig'
 import FarmTabs from "../components/FarmTabs";
 import AnimalPopover from "../components/AnimalPopover";
 import "./AnimalTable.css";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, useGridApiRef } from '@mui/x-data-grid';
 import TableContainer from '@mui/material/TableContainer';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+import EditIcon from '@mui/icons-material/Edit';
+
 import AnimalCreator from "../components/AnimalCreator";
 
 import { getConfig } from '../api/getToken';
 import { Link } from "react-router-dom";
 import { set } from "date-fns";
+import { FormControlLabel, FormGroup, FormControl, FormHelperText, IconButton } from "@mui/material";
+import Autocomplete from '@mui/material/Autocomplete';
 
 const AnimalTable = ({farms}) => {
     const [animalList, setAnimalList] = useState([]); /* The State for the list of animals. The initial state is [] */
@@ -23,10 +32,11 @@ const AnimalTable = ({farms}) => {
     const [filterModel, setFilterModel] = useState({items: []});
     const [schemas, setSchemas] = useState([]);
     const [selectedSchema, setSelectedSchema] = useState(null);
+    const [newAnimal, setNewAnimal] = useState( useState({name: '', type: '', father: '', mother: '', male: true, alive: true, fields: {}}));
 
     const token = getConfig();
 
-    //useEffect(displayAll,[clear])
+    const gridApi = useGridApiRef();
 
     function displayAll() {
         (async () => {
@@ -81,17 +91,15 @@ const AnimalTable = ({farms}) => {
         })()
     }, [])
 
-    useEffect(() => {
-        //setAnimalList(animalList.filter((animal)=>{animal.farms.includes(farm)}))
-    },[farm])
-
 
     function calculateColumnsAndRows(schema) {
         let newCols = defaultCols;
 
         if (schema) {
             for (let key in schema._fields) {
-                newCols.push({field: key, headerName: key, headerClassName: 'grid-header', headerAlign: 'left', flex: 1});
+                newCols.push({field: key, headerName: key, headerClassName: 'grid-header', headerAlign: 'left', flex: 1, editable: true, renderEditCell: (params) => {
+                    return fieldTypeSwitch(schema._fields[key]._type, params);
+                }});
             }
         }
  
@@ -143,17 +151,133 @@ const AnimalTable = ({farms}) => {
     }, [animalList])
 
 
+    const fieldTypeSwitch = (type, params) => {
+        switch(type) { /* check the type of the field and display appropriate input method */
+            case "java.lang.Boolean":
+                return (
+                    <FormControl sx={{width: '100%'}}>
+                    <Select
+                        value={params.value}
+                        onChange={(e) => {
+                            gridApi.current.setEditCellValue({id: params.id, field: params.field, value: e.target.value});
+                        }}
+                    >
+                        <MenuItem value={''}><em>Empty</em></MenuItem>
+                        <MenuItem value={true}>Yes</MenuItem>
+                        <MenuItem value={false}>No</MenuItem>
+                    </Select>
+                    </FormControl>
+                );
+            case "java.lang.String":
+                return (
+                    <FormControl sx={{width: '100%'}}>
+                    <TextField
+                        fullWidth
+                        placeholder={params.value}
+                        onChange={(e) => {
+                            gridApi.current.setEditCellValue({id: params.id, field: params.field, value: e.target.value});
+                        }}
+                    />
+                    </FormControl>
+                );
+            case "java.lang.Integer":
+                return (
+                    <FormControl sx={{width: '100%'}}>
+                    <TextField
+                        type='number'
+                        fullWidth
+                        placeholder={params.value}
+                        onChange={(e) => {
+                            gridApi.current.setEditCellValue({id: params.id, field: params.field, value: e.target.value});
+                        }}
+                    />
+                    </FormControl>
+                );
+            case "java.lang.Double":
+                return (
+                    <FormControl  sx={{width: '100%'}}>
+                    <TextField
+                        type='number'
+                        fullWidth
+                        placeholder={params.value}
+                        onChange={(e) => {
+                            gridApi.current.setEditCellValue({id: params.id, field: params.field, value: e.target.value});
+                        }}
+                    />
+                    </FormControl>
+                );
+            case "java.time.ZonedDateTime":
+                return (
+                    <FormControl sx={{width: '100%'}}>
+                    <DatePicker
+                        onChange={(e) => {
+                            gridApi.current.setEditCellValue({id: params.id, field: params.field, value: e.target.value});
+                        }}
+                        slotProps={{textField: {fullWidth: true}}}
+                    />
+                    </FormControl>
+                )
+            default:
+                return <></>;
+        };
+    }
+
     const defaultCols = [
-        { field: 'name', headerName: 'Name', headerClassName: 'grid-header', headerAlign: 'left', flex: 1,
-            renderCell: (animal) => {return <AnimalPopover animalID={animal.value._id}/>} },
+        { field: 'name', headerName: 'Name', headerClassName: 'grid-header', headerAlign: 'left', flex: 1, editable: true,
+            renderCell: (animal) => {return <AnimalPopover animalID={animal.value._id}/>}, renderEditCell: (params) => {
+                params.value = params.value.name;
+                return fieldTypeSwitch("java.lang.String", params);
+            } },
         { field: 'type', headerName: 'Type', headerClassName: 'grid-header', headerAlign: 'left', flex: 1 },
-        { field: 'father', headerName: 'Father', headerClassName: 'grid-header', headerAlign: 'left', flex: 1,
+        { field: 'father', headerName: 'Father', headerClassName: 'grid-header', headerAlign: 'left', flex: 1, editable: true,
         renderCell:(animal)=>{return animal.value.father?
-             <AnimalPopover key={animal.value.father} animalID={animal.value.father}/> : "Unregistered"}},
-        { field: 'mother', headerName: 'Mother', headerClassName: 'grid-header', headerAlign: 'left', flex: 1,
+             <AnimalPopover key={animal.value.father} animalID={animal.value.father}/> : "Unregistered"}, renderEditCell: (params) => {
+                return (
+                    <Autocomplete
+                                    style={{width: '100%'}}
+                                    size='medium'
+                                    renderOption={(props, option) => {
+                                        return (
+                                            <li {...props} key={option.id}>
+                                                {option.name}
+                                            </li>
+                                        );
+                                    }}
+                                    renderInput={(params) => <TextField {...params} label="Father"/>}
+                                    isOptionEqualToValue={(option, value) => option.id === value.id}
+                                    getOptionLabel={option => option.name}
+                                    options={
+                                        animalList.filter((animal)=>{return animal.type.toLowerCase() === params.row.type.toLowerCase() && animal.male === true && animal._id !== params.row.id}).map((animal)=>{
+                                            return {id: animal._id, name: animal.name}
+                                        })
+                                    }
+                    />
+                );
+            }},
+        { field: 'mother', headerName: 'Mother', headerClassName: 'grid-header', headerAlign: 'left', flex: 1, editable: true,
             renderCell:(animal)=>{return animal.value.mother?
-                <AnimalPopover key={animal.value.mother} animalID={animal.value.mother}/> : "Unregistered"}},
-        { field: 'sex', headerName: 'Sex', headerClassName: 'grid-header', headerAlign: 'left', flex: 1 },
+                <AnimalPopover key={animal.value.mother} animalID={animal.value.mother}/> : "Unregistered"}, renderEditCell: (params) => {
+                    return (<Autocomplete
+                        style={{width: '100%'}}
+                        size='medium'
+                        renderOption={(props, option) => {
+                            return (
+                                <li {...props} key={option.id}>
+                                    {option.name}
+                                </li>
+                            );
+                        }}
+                        renderInput={(params) => <TextField {...params} label="Mother"/>}
+                        isOptionEqualToValue={(option, value) => option.id === value.id}
+                        getOptionLabel={option => option.name}
+                        options={
+                            animalList.filter((animal)=>{console.log(animal);return animal.type.toLowerCase() === params.row.type.toLowerCase() && animal.male === false && animal._id !== params.row.id}).map((animal)=>{
+                                return {id: animal._id, name: animal.name}
+                            })
+                        }
+                    />)
+                }},
+        { field: 'sex', headerName: 'Sex', headerClassName: 'grid-header', headerAlign: 'left', flex: 1, editable: true },
     ];
     const [cols, setCols] = useState(defaultCols);
 
@@ -169,7 +293,19 @@ const AnimalTable = ({farms}) => {
             <FarmTabs farms={farms} selectedFarm={farm} setSelectedFarm={setFarm}/>
         </span>
         <Paper style={{height: 'calc(100% - 525px)', marginBottom: '20px'}}>
-            <DataGrid disableRowSelectionOnClick filterModel={filterModel} style={{fontSize: '1rem'}} checkboxSelection columns={cols} rows={rows} onCellClick={(params, event, details) => {
+            <DataGrid apiRef={gridApi} isCellEditable={() => true} disableRowSelectionOnClick filterModel={filterModel} style={{fontSize: '1rem'}} checkboxSelection columns={[...cols, {
+                field: 'edit',
+                headerName: 'Edit',
+                headerClassName: 'grid-header',
+                headerAlign: 'right',
+                flex: 0.1,
+                renderCell: (params) => {
+                    return <IconButton onClick={() => {
+                        gridApi.current.startRowEditMode({ id: params.id });
+                    }}><EditIcon/></IconButton>
+                    
+                }
+            }]} rows={rows} onCellClick={(params, event, details) => {
                 if (params.field === 'type') {
                     setFilterModel({
                         items: [
@@ -187,7 +323,7 @@ const AnimalTable = ({farms}) => {
                 }
             }}/>
         </Paper>
-        <div style={{margin: '1%', display: 'flex'}}>
+        <div style={{marginTop: '1%', display: 'flex'}}>            
             <Button variant="contained" onClick={() => {
                 setFilterModel({items: []});
                 setSelectedSchema(null);
