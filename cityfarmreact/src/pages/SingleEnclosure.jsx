b3488553a3792b66f9f37b89ce01e339fc30b1c3
os22128 <os22128@bristol.ac.uk>
2024-04-07 18:38:25 +0100

    showing all the animals and their types almost works!

--- cityfarmreact/src/pages/SingleEnclosure.jsx
+++ cityfarmreact/src/pages/SingleEnclosure.jsx
@@ -1,7 +1,8 @@
 import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "../api/axiosConfig";
 import {getConfig} from "../api/getToken";
+import AnimalPopover from "../components/AnimalPopover";
 
 const SingleEnclosure = (props) => {
   const token = getConfig();
@@ -10,12 +11,14 @@ const SingleEnclosure = (props) => {
   const [animalTypes,setAnimalTypes] = useState([])
 
 
+
   useEffect(() => {
     (
         async () => {
     try {
       const req = await axios.get(`/enclosures/by_id/${enclosureID}`, token)
       setEnclosure(req.data)
+
 
     } catch (error) {
       if (error.response.status === 401) {
@@ -28,8 +31,58 @@ const SingleEnclosure = (props) => {
   })();
   }, [enclosureID])
 
-  return (<div>{enclosure.name}</div>)
+
+  const animalsByType = (type)=> {
+    let animals = []
+    for (const animal of enclosure.holding) {
+      if (animal.type === type) {
+        animals.push(animal)
+      }
+      return animals
+    }
+  }
+  const holdings =()=>{
+    let holdingDisplay=[]
+    if (enclosure.holding!==undefined){
+      for (const animal of enclosure.holding){
+        if (animalTypes.indexOf(animal.type)=== -1){
+          setAnimalTypes([...animalTypes,animal.type])
+        }
+      }
+      for (const type of animalTypes){
+        holdingDisplay.push(
+            <>
+              {type}
+              {animalsByType(type).map((animal) => (
+               <AnimalPopover key={animal._id} animalID={animal._id}/>
+       ))}
+            </>
+        )
+
+      }
+    }
+    return holdingDisplay
+  }
+
+  return (
+  <div>
+    {enclosure.name}
+    {holdings().map((item)=>(item))}
+
+  </div>)
 
 }
 
+
+//FOR WHEN TYPES ARE COOLER
+// for (const [type,animalList] of Object.entries(enclosure.holding)){
+//   console.log([type,animalList])
+//     holdingDisplay.push(<>
+//         <h4>{type}</h4>
+//         <div>
+//           {animalList.map((animal) => (
+//               <AnimalPopover key={animal} animalID={animal}/>
+//           ))}
+//         </div>
+// </>)
 export default SingleEnclosure
